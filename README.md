de7e0b7e81efb1db0c68896a225538b930621d1d
Richard Littauer <richard.littauer@gmail.com>
2015-11-08 10:28:36 -0500

    Linted, made one-call command #3

--- README.md
+++ README.md
@@ -3,11 +3,19 @@ Make a markdown list of a user's or organization's repositories
 
 # Usage
 
-* Manually enter in your username and password in index.js. 
+Manually enter in your username and password in index.js. 
+
+To see the individual calls:
 
 ```sh
   node index.js --getOrgRepos=ExampleOrganization
   node index.js --parse=ExampleOrganization-repositories.txt
+```
+
+Else, just do:
+
+```sh
+  node index.js <organization_name>
 ```
 
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var Github = require("github-api")
-var fs = require("fs")
-var _ = require("lodash")
+var Github = require('github-api')
+var fs = require('fs')
+var _ = require('lodash')
 var argv = require('minimist')(process.argv.slice(2), {
   alias: {
     o: getOrgRepos,
@@ -12,48 +12,59 @@ var argv = require('minimist')(process.argv.slice(2), {
 var github = new Github({
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
-  auth: "basic"
-});
+  auth: 'basic'
+})
 
-var getOrgRepos = function(orgname) {
-	var user = github.getUser();
-	user.orgRepos(orgname, function(err, repos) {
-		fs.writeFile(orgname + '-repositories.txt', JSON.stringify(repos), function (err) {
-		  if (err) throw err;
-		  console.log('It\'s saved!');
-		});
-	});
-}
-
-var getRepos = function() {
-  var user = github.getUser();
-  user.repos(function(err, repos) {
-    fs.writeFile(github.username + '-repositories.txt', JSON.stringify(repos), function (err) {
-      if (err) throw err;
-      console.log('It\'s saved!');
-    });
-  });
-}
-
-var parseJson = function(filename) {
-	fs.readFile(filename, function(err, data) {
-    if (err) console.log("Failed to parse file.")
-
-    data = JSON.parse(data.toString())
-
-    _.forEach(data, function(repo) {
-
-      var string = '* [' + repo.name + ']('
-      string += repo.html_url + ') ' + repo.description
-      if (repo.fork && repo.homepage)
-        string += ' (_Fork_)'
-
-      console.log(string)
+var getOrgRepos = function (orgname) {
+  var user = github.getUser()
+  user.orgRepos(orgname, function (err, repos) {
+    if (err) throw err
+    fs.writeFile(orgname + '-repositories.txt', JSON.stringify(repos), function (err) {
+      if (err) throw err
+      console.log('It\'s saved!')
     })
-
   })
 }
 
-if (argv.getOrgRepos) getOrgRepos(argv.getOrgRepos)
+var getRepos = function () {
+  var user = github.getUser()
+  user.repos(function (err, repos) {
+    if (err) throw err
+    fs.writeFile(github.username + '-repositories.txt', JSON.stringify(repos), function (err) {
+      if (err) throw err
+      console.log('It\'s saved!')
+    })
+  })
+}
+
+var printString = function (data) {
+  _.forEach(data, function (repo) {
+    var string = '* [' + repo.name + ']('
+    string += repo.html_url + ') ' + repo.description
+    if (repo.fork && repo.homepage) {
+      string += ' (_Fork_)'
+    }
+
+    console.log(string)
+  })
+}
+
+var parseJson = function (filename) {
+  fs.readFile(filename, function (err, data) {
+    if (err) console.log('Failed to parse file.')
+    printString(JSON.parse(data.toString()))
+  })
+}
+
+var printRepos = function (orgname) {
+  var user = github.getUser()
+  user.orgRepos(orgname, function (err, repos) {
+    if (err) console.log('Null')
+    printString(repos)
+  })
+}
+
+if (argv.getOrgRepos)getOrgRepos(argv.getOrgRepos)
 if (argv.getRepos) getRepos()
 if (argv.parse) parseJson(argv.parse)
+if (argv._) _.forEach(argv._, printRepos(argv._))
